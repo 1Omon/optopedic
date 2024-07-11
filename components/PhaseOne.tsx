67e681e9a0a22943ada8ded9be808f8ba545a444
1Omon <1serwise@gmail.com>
2024-07-11 09:56:28 +0000

    optometry-project

---
+++ components/PhaseOne.tsx
@@ -0,0 +1,35 @@
+import React from 'react'
+
+const PhaseOne = () => {
+  return (
+    <div className='w-full grid grid-rows-5 grid-cols-6 gap-5'>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+      <div className="bg-pink-200 w-[200px] h-[200px]"></div>
+    </div>
+  )
+}
+
+export default PhaseOne
